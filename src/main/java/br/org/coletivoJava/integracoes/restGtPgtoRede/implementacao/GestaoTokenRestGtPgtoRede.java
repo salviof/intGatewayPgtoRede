fdf190436a9f1a9825052dcbcbddf5b806d91cdf
Salvio Furbino <salviof@gmail.com>
2020-12-04 18:46:37 -0300

    Atualizacao automática

--- src/main/java/br/org/coletivoJava/integracoes/restGtPgtoRede/implementacao/GestaoTokenRestGtPgtoRede.java
+++ src/main/java/br/org/coletivoJava/integracoes/restGtPgtoRede/implementacao/GestaoTokenRestGtPgtoRede.java
@@ -3,22 +3,12 @@ package br.org.coletivoJava.integracoes.restGtPgtoRede.implementacao;
 import br.org.coletivoJava.integracoes.restGtPgtoRede.api.InfoIntegracaoRestGtPgtoRedeTransacoes;
 import br.org.coletivoJava.integracoes.gatewayPgto.api.FabApiRestGatewayPgtoRedeTransacoes;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.implementacao.gestaoToken.GestaoTokenChaveUnica;
-import org.json.simple.JSONObject;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.api.FabTipoAgenteClienteRest;
+import com.super_bits.modulosSB.SBCore.integracao.libRestClient.api.token.ItfTokenDeAcessoExterno;
 import com.super_bits.modulosSB.SBCore.modulos.objetos.registro.Interfaces.basico.ItfUsuario;
 
 @InfoIntegracaoRestGtPgtoRedeTransacoes(tipo = FabApiRestGatewayPgtoRedeTransacoes.EFETUAR_PAGAMENTO_CAPTURA_IMEDIATA)
 public class GestaoTokenRestGtPgtoRede extends GestaoTokenChaveUnica {
-
-    @Override
-    public String gerarNovoToken() {
-        return null;
-    }
-
-    @Override
-    public String extrairToken(JSONObject jsonObject) {
-        return null;
-    }
 
     @Override
     public boolean validarToken() {
@@ -30,4 +20,15 @@ public class GestaoTokenRestGtPgtoRede extends GestaoTokenChaveUnica {
             final ItfUsuario pUsuario) {
         super(FabApiRestGatewayPgtoRedeTransacoes.class, pTipoAgente, pUsuario);
     }
+
+    @Override
+    public ItfTokenDeAcessoExterno gerarNovoToken() {
+        return getTokenCompleto();
+    }
+
+    @Override
+    public ItfTokenDeAcessoExterno loadTokenArmazenado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
